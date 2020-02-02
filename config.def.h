6f726a39096529dd8038fb2c2b50f646cac3a801
tyler pennington <tylerpnn@gmail.com>
2020-02-02 02:11:00 -0800

    add implacerotate

--- config.def.h
+++ config.def.h
@@ -73,6 +73,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_j,      inplacerotate,  {.i = -1} },
+	{ MODKEY|ShiftMask,             XK_k,      inplacerotate,  {.i = +1} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
